cd21348f44aa52e80b4f9e77eaef1b78bea22b13
Harry Hoang <theanhhoang0201@gmail.com>
2026-03-15 12:35:20 -0600

    forgot to save header file for motion_model

--- nav_ws/src/fastslam/include/fastslam/motion_model.hpp
+++ nav_ws/src/fastslam/include/fastslam/motion_model.hpp
@@ -17,7 +17,7 @@ namespace fastslam
             explicit MotionModel(double a1, double a2, double a3, double a4) : 
                 alpha_1_(a1), alpha_2_(a2), alpha_3_(a3), alpha_4_(a4) {}
             
-            Pose applyMotionModel(Pose robot_pose, Pose prev_pose, Pose curr_pose); 
+            Pose applyMotionModel(Pose robot_pose, Pose prev_odom, Pose curr_odom); 
 
         private: 
             double alpha_1_, alpha_2_, alpha_3_, alpha_4_;
