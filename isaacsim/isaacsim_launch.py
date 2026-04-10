044c93c38005eb5c60db5e63683d25096e57ac68
Harry Hoang <theanhhoang0201@gmail.com>
2026-04-10 10:45:41 -0600

    add office environment and wheel material assets

--- isaacsim/isaacsim_launch.py
+++ isaacsim/isaacsim_launch.py
@@ -43,7 +43,7 @@ import omni.graph.core as og
 
 
 class LaunchSim(): 
-    ENV_URL = "/home/harry/dev/nav_isaacsim/world/simple_obstacles.usd"
+    ENV_URL = "/home/harry/dev/nav_isaacsim/world/office.usd"
     
     def __init__(self):
         self._world = None
